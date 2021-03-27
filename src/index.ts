95fd0ec96711436903aff3ec4c98da3b35936f35
José Luis Pereira <pepelui@outlook.com>
2021-03-26 21:46:00 -0300

    Added types, methods and generate menu

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-import "./styles.css"
+import "./styles.css";
+import { RadialMenu, radialMenuOptions } from './radialMenu'
+
+const options: radialMenuOptions = {
+  width: 400,
+  height: 400,
+  slices: [
+    {
+      text: 'test 1'
+    },
+    {
+      text: 'test 2'
+    },
+    {
+      text: 'test 3'
+    },
+    {
+      text: 'test 4'
+    },
+  ]
+}
+
+const element = document.querySelector('#app') as HTMLElement
+const radial = new RadialMenu(element, options)
--- src/radialMenu.ts
+++ src/radialMenu.ts
@@ -1,19 +1,85 @@
-export class RadialMenu {
-  SVGElement: HTMLOrSVGElement
-  parentElement: HTMLElement
+export type sliceOption = {
+  radius?: number | unknown
+  text?: string
+  icon?: string | unknown,
+  link?: boolean
+}
+export type radialMenuOptions = {
+  height: number
+  width: number
+  slices: Array<sliceOption>
+}
 
-  constructor (element: HTMLElement, options: Object) {
+export class RadialMenu {
+  SVGElement: SVGElement
+  parentElement: HTMLElement
+  options: Object
+  SVGSlices: Array<SVGElement> = []
+  width: number
+  height: number
+  slices: Array<sliceOption>
+
+
+  constructor (element: HTMLElement, opt: radialMenuOptions) {
+    const { width, height, slices } = opt
+
     this.SVGElement = document.createElementNS("http://www.w3.org/2000/svg", "svg")
+    this.SVGElement.setAttribute('width', `${width}px`)
+    this.SVGElement.setAttribute('height', `${height}px`)
     this.parentElement = element
+    this.options = opt
+    this.width = width
+    this.height = height
+    this.slices = slices
+
+    this.generateMenu()
   }
 
-  describeArc (x: number, y: number, radius: number, spread: number, startAngle: number, endAngle: number){
+  generateMenu (): void {
+    this.SVGSlices = this.generateSlices()
+    this.SVGSlices.forEach(element => {
+      (<any>this.SVGElement).appendChild(element)
+    })
+    this.parentElement.appendChild(this.SVGElement)
+  }
+
+  private generateSlices (): Array<SVGElement> {
+    const radiusSlice: number = 360 / this.slices.length
+    const svgPaths: Array<SVGElement> = []
+    let radiusStart = 0
+    let radiusEnd = radiusSlice
+
+
+    this.slices.forEach((slice: sliceOption): void => {
+      console.log(`start: ${radiusStart}`)
+      console.log(`end: ${radiusEnd}`)
+      svgPaths.push(this.createSlice(this.describeArc(this.width / 2, this.height / 2, 50, 130, radiusStart, radiusEnd)))
+      radiusEnd = radiusEnd + radiusSlice
+      radiusStart = radiusStart + radiusSlice
+    })
+
+    return svgPaths
+  }
+
+  private createSlice (arcPath: string): SVGElement {
+    const pathElement = document.createElementNS("http://www.w3.org/2000/svg", 'path')
+    pathElement.setAttribute('d', arcPath)
+    pathElement.setAttribute('fill', 'blue')
+    pathElement.setAttribute('stroke', 'white')
+    pathElement.setAttribute('stroke-opacity', '1')
+    pathElement.setAttribute('stroke-width', '3')
+    
+
+    return pathElement
+  }
+
+  private describeArc (x: number, y: number, radius: number, spread: number, startAngle: number, endAngle: number): string {
     const innerStart = this.polarToCartesian(x, y, radius, endAngle)
     const innerEnd = this.polarToCartesian(x, y, radius, startAngle)
     const outerStart = this.polarToCartesian(x, y, radius + spread, endAngle)
     const outerEnd = this.polarToCartesian(x, y, radius + spread, startAngle)
   
-    const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+    const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1"
   
     var d = [
         "M", outerStart.x, outerStart.y,
@@ -27,7 +93,7 @@ export class RadialMenu {
   }
 
   private polarToCartesian(centerX: number, centerY: number, radius: number, angleInDegrees: number) {
-    var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+    const angleInRadians = (angleInDegrees-90) * Math.PI / 180.0
     
     return {
       x: centerX + (radius * Math.cos(angleInRadians)),
